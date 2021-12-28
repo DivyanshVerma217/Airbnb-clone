90f0644c8f80b3b402e23933fd65073699f530b8
Divyansh Verma <69747865+DivyanshVerma217@users.noreply.github.com>
2021-12-28 17:13:01 +0530

    Add files via upload

---
+++ Components/Footer.js
@@ -0,0 +1,60 @@
+function Footer(){
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10
+        px-32 py-14 bg-gray-100 text-gray-600">
+
+            <div className="space-y-4 text-gray-800 text-xs">
+
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How it works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+
+
+            </div>
+
+
+            <div className="space-y-4 text-gray-800 text-xs">
+
+                <h5 className="font-bold">HOST</h5>
+                <p>Divyansh Verma</p>
+                <p>Presents</p>
+                <p>Airbnb-clone</p>
+                
+
+
+            </div>
+
+            <div className="space-y-4 text-gray-800 text-xs">
+
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help center</p>
+                <p>Trust & Safety</p>
+                <p>We love you </p>
+                <p>We care</p>
+                
+
+
+            </div>
+
+            <div className="space-y-4 text-gray-800 text-xs">
+
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not the real site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>DivyanshFam</p>
+
+
+            </div>
+
+          
+
+            
+        </div>
+    )
+}
+export default Footer;
